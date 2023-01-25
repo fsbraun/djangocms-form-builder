a01b479eb84f14bf207057bb9159000f6d5eb46c
Stefan van den Eertwegh <34129243+svandeneertwegh@users.noreply.github.com>
2023-01-25 16:38:13 +0100

    Update djangocms_form_builder/cms_plugins/legacy.py
    indeed import not needed to change
    
    Co-authored-by: Fabian Braun <fsbraun@gmx.de>

--- djangocms_form_builder/cms_plugins/legacy.py
+++ djangocms_form_builder/cms_plugins/legacy.py
@@ -2,7 +2,7 @@ from cms.plugin_pool import plugin_pool
 from django.conf import settings
 
 if 'djangocms_frontend.contrib.frontend_forms' in settings.INSTALLED_APPS:
-    from djangocms_frontend.contrib.content.cms_plugins import (
+    from djangocms_frontend.contrib.frontend_forms.cms_plugins import (
         BooleanFieldPlugin,
         CharFieldPlugin,
         ChoicePlugin,
